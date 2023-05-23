717c85c603f6245208ad17e1972a13034579612f
200714 <200714@e0601-11.JC-21.local>
2023-05-23 11:22:44 +0900

    テスト

---
+++ src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
@@ -0,0 +1,15 @@
+package jp.te4a.spring.boot.myapp5;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+	@RequestMapping("/")
+	public String index(Model model) {
+	//return "this is Spring Boot sample.";
+	return "index";
+	}
+
+}
