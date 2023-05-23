bc11dc4948fa34c251497eafd77dba2d7749966d
200714 <200714@e0601-11.JC-21.local>
2023-05-23 12:12:37 +0900

    a

--- src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 	@RequestMapping("/")
 	public String index(Model model) {
 	//return "this is Spring Boot sample.";
-		model.addAttribute("msg","this is a setting message");
+		model.addAttribute("msg","this is setting message");
 	return "index";
 	}
 }
