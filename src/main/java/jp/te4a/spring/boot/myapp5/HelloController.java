db2e2bca31d20d696fad8c037dc9f1f91c40b4a2
200714 <200714@e0601-11.JC-21.local>
2023-05-23 12:21:50 +0900

    a

--- src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
@@ -3,6 +3,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -13,7 +15,7 @@ public class HelloController {
 	return "index";
 	}
 	@RequestMapping(value="/post",method=RequestMethod.POST)
-	public modelAndView postForm(@requestParam("text1") String text1) {
+	public ModelAndView postForm(@RequestParam("text1") String text1) {
 	ModelAndView mv = new ModelAndView("index");
 	mv.addObject("msg", "you write '" + text1 + "'!!!");
 	return mv;
