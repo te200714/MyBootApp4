b7eb34272393b0663b7c0a38632b920cb8acdfa9
200714 <200714@e0601-11.JC-21.local>
2023-05-23 11:25:52 +0900

    あ

--- src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
@@ -2,7 +2,6 @@ package jp.te4a.spring.boot.myapp5;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
